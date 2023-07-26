12839bf780496c8a80e85a4b2bdfecf958b9f2a4
monugarg01 <140064097+monugarg01@users.noreply.github.com>
2023-07-27 01:24:58 +0530

    78e456t34ejhg

--- src/component/card.js
+++ src/component/card.js
@@ -19,7 +19,7 @@ useEffect(()=>{
     getPostsData();
 })
 const getPostsData = () => {
-    axios.get("https://15.206.148.51:4000/test")
+    axios.get("http://15.206.148.51:4000/test")
     .then(data => setData(data.data))
     .catch(error => console.log(error));
     };  
