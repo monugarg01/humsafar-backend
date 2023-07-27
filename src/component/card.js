5b2cc6ca14c513951b45ceb3d1aa2cdfd041e2cc
monugarg01 <140064097+monugarg01@users.noreply.github.com>
2023-07-27 22:30:01 +0530

    minor-fix

--- src/component/card.js
+++ src/component/card.js
@@ -19,7 +19,7 @@ useEffect(()=>{
     getPostsData();
 })
 const getPostsData = () => {
-    axios.get("https://api.humsafarpredictions.in/test")
+    axios.get("https://api.humsafarpredictions.in/test/")
     .then(data => setData(data.data))
     .catch(error => console.log(error));
     };  
